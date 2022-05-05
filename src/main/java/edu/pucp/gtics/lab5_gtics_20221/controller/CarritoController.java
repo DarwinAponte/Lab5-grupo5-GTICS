c9f8ae46e7efbc09148e01d2a41d213b3dabe080
Rodrigo Loayza <loayza.rs@pucp.edu.pe>
2022-05-04 20:05:25 -0500

    Carrito controller

--- src/main/java/edu/pucp/gtics/lab5_gtics_20221/controller/CarritoController.java
+++ src/main/java/edu/pucp/gtics/lab5_gtics_20221/controller/CarritoController.java
@@ -10,36 +10,73 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
+@Controller
 @RequestMapping("/carrito")
 public class CarritoController {
 
+    @Autowired
+    JuegosRepository juegosRepository;
 
-    @GetMapping("/listaCarrito")
-    public String listaCarrito (Model model){
+    @Autowired
+    JuegosxUsuarioRepository juegosxUsuarioRepository;
 
+    @GetMapping("/lista")
+    public String listaCarrito (Model model, HttpSession session) {
+        ArrayList<Juegos> carrito = (ArrayList<Juegos>) session.getAttribute("carrito");
+        carrito.sort(Comparator.comparing(Juegos::getPrecio));
+        model.addAttribute("listaJuegos", carrito);
         return "carrito/lista";
     }
 
-    public String nuevoCarrito(@RequestParam("id") int id){
-
+    @GetMapping("/compra")
+    public String comprarCarrito (@RequestParam("id") int id, HttpSession session, RedirectAttributes attr){
+        ArrayList<Juegos> carrito = (ArrayList<Juegos>) session.getAttribute("carrito");
+        for (Juegos juego : carrito) {
+            juegosxUsuarioRepository.registrarJuegoPorUser(id, juego.getIdjuego()); // la cantidad 1 se añade en el query
+        }
+        //el carrito se borra una vez se hace la compra
+        carrito = new ArrayList<>();
+        session.setAttribute("carrito", carrito);
+        session.setAttribute("ncarrito", carrito.size());
+        attr.addFlashAttribute("msg", "Compra exitosa!");
         return "redirect:/vista";
     }
 
-    public String editarCarrito(  ){
-
-        return "redirect:/juegos/lista";
+    @GetMapping("/anadir")
+    public String anadirCarrito (@RequestParam("id") int idJuego, HttpSession session, RedirectAttributes attr) {
+        ArrayList<Juegos> carrito = (ArrayList<Juegos>) session.getAttribute("carrito");
+        Optional<Juegos> optionalJuego = juegosRepository.findById(idJuego);
+        if (optionalJuego.isPresent()) {
+            Juegos juegoCarrito = optionalJuego.get();
+            carrito.add(juegoCarrito);
+        }
+        session.setAttribute("carrito", carrito);
+        session.setAttribute("ncarrito", carrito.size());
+        attr.addFlashAttribute("msg", "Juego añadido al carrito");
+        return "redirect:/vista";
     }
 
-    public String borrarCarrito(){
-
+    @GetMapping("/borrar")
+    public String borrarCarrito (@RequestParam("id") int idJuegoDel, HttpSession session, RedirectAttributes attr) {
+        ArrayList<Juegos> carrito = (ArrayList<Juegos>) session.getAttribute("carrito");
+        for (Juegos juegoDel : carrito) {
+            if (juegoDel.getIdjuego() == idJuegoDel) {
+                carrito.remove(juegoDel);
+                break;
+            }
+        }
+        session.setAttribute("carrito", carrito);
+        session.setAttribute("ncarrito", carrito.size());
+        attr.addFlashAttribute("msg", "Juego eliminado del carrito");
         return "redirect:/carrito/lista";
     }
-
 
 }
